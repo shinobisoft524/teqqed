34fb69277bc7fb3abc50e6ca00c8289ef99de391
shinobisoft524 <49074891+shinobisoft524@users.noreply.github.com>
2024-08-20 12:52:52 +0900

    Font fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 import './globals.css';
 import Providers from './providers';
 import type React from 'react';
+import { Inter } from 'next/font/google'
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export default function RootLayout({
   children
@@ -8,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       <body className="flex min-h-screen w-full flex-col">
         <Providers>{children}</Providers>
       </body>
