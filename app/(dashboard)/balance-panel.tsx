9424daa9d4f9e21366c340bf9f822079032c50e0
shinobisoft524 <49074891+shinobisoft524@users.noreply.github.com>
2024-08-21 00:50:28 +0900

    Fix balance font

--- app/(dashboard)/balance-panel.tsx
+++ app/(dashboard)/balance-panel.tsx
@@ -1,6 +1,9 @@
 
 import navStyles from '@/styles/nav.module.css';
 import clsx from 'clsx';
+import {
+  Plus,
+} from 'lucide-react';
 
 export default function BalancePanel(props: {}) {
   return (
@@ -9,9 +12,9 @@ export default function BalancePanel(props: {}) {
         `${navStyles.navOverview} mr-auto flex flex-row p-3 gap-2 h-8 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-neutral-200 md:h-8`
       )}
     >
-      <span className="flex h-5">{`Balance`}</span>
-      <span className="flex h-5">{`$122,00`}</span>
-      <span className="flex h-5">{`+`}</span>
+      <span className="flex text-sm font-medium text-neutral-500">{`Balance`}</span>
+      <span className="flex text-sm font-medium text-neutral-200">{`$122,00`}</span>
+      <span className="flex text-amber-300 "><Plus className='w-4 h-4'/></span>
     </div>
   );
 }
